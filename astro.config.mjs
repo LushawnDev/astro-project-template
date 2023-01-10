f26ce63620bb9e64f2fe26d6c63dfd6f83d2c586
LushawnDev <lucianchevvy97@gmail.com>
2023-01-10 13:59:44 +0000

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "astro/config";
+// import NetlifyCMS from "astro-netlify-cms";
+import { astroImageTools } from "astro-imagetools";
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    astroImageTools,
+    prefetch(),
+    sitemap(),
+    robotsTxt(),
+    compress({ img: false, svg: false }),
+  ],
+});
