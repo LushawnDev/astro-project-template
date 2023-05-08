14d2056c3f96546e0b260a212db22a1541479599
LushawnDev <lucianchevvy97@gmail.com>
2023-05-08 12:53:29 +0100

    Added site variable and added partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
 import { defineConfig } from "astro/config";
-// import NetlifyCMS from "astro-netlify-cms";
 import { astroImageTools } from "astro-imagetools";
 import robotsTxt from "astro-robots-txt";
-
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import compress from "astro-compress";
-
-// https://astro.build/config
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, prefetch(), sitemap(), robotsTxt(), compress({
-    img: false,
-    svg: false
-  })]
+  site: "https://ENTER-SITENAME.com",
+  integrations: [
+    astroImageTools,
+    prefetch(),
+    sitemap(),
+    robotsTxt(),
+    partytown(),
+    compress({
+      img: false,
+      svg: false,
+    }),
+  ],
 });
